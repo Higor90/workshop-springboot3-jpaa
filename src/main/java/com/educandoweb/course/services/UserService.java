ae488b09eb7921c452641ce411e200ca2234b769
Higor Nobrega <nobregahigor@gmail.com>
2023-11-01 16:37:48 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		java.util.Optional<User> obj  = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }	
